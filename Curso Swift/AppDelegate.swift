dbd14bd9a7bea657a8df829ed09fa105b0246907
Jaime Salcedo Rivas <jaimesalcedorivas@macbook-pro-de-jaime.local>
2016-10-07 20:20:39 -0500

    Initial Commit

---
+++ Curso Swift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Curso Swift
+//
+//  Created by Jaime Salcedo Rivas on 07/10/16.
+//  Copyright © 2016 Jaime Salcedo Rivas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Curso Swift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Curso Swift
+//
+//  Created by Jaime Salcedo Rivas on 07/10/16.
+//  Copyright © 2016 Jaime Salcedo Rivas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
